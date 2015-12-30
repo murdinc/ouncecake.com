c886c08358a8b532c10a466ce1c33aa600627fa8
Ahmad Abdo <aabdo@salon.com>
2015-12-30 12:26:55 -0800

    display none!

--- ouncecake.js
+++ ouncecake.js
@@ -1,6 +1,11 @@
 // Switch the content divs when a header link is clicked!
 
 $('.link').click(function() {
+
   $('.content').css('visibility', 'hidden');
+  $('.content').css('display', 'none');
+
   $('#'+$(this).attr('name')).css('visibility', 'visible');
+  $('#'+$(this).attr('name')).css('display', 'block');
+
 })
